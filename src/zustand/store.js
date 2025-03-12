79a6f0fa3d2a843a76d5a7a3a48b19a873d4bae2
Charlotte Ganske <cganske04@icloud.com>
2025-03-11 20:08:05 -0500

    initial zustand store added

---
+++ src/zustand/store.js
@@ -0,0 +1,41 @@
+import { create } from 'zustand';
+import axios from 'axios';
+
+const useStore = create((set, get) => ({
+    //use set({}) on these to change their data
+    order: [],
+    pizza: [],
+
+    fetchPizza: async () => {
+
+        try {
+            const response = await axios.get(
+                "/api/pizza"
+            );
+            //variable to hold response
+            const pizza = response.data;
+            console.log("got pizza: ", pizza);
+            set({ pizza: pizza});
+        } catch (error) {
+            console.log("Error in fetchPizza")
+        }
+    }, //end fetchPizza
+
+    fetchOrder: async () => {
+
+        try {
+            const response = await axios.get(
+                "/api/order"
+            );
+            //variable to hold response
+            const order = response.data;
+            console.log("got order: ", order);
+            set({ order: order});
+        } catch (error) {
+            console.log("Error in fetchOrder")
+        }
+    }, //end fetchPizza
+
+
+
+})) //end useStore
